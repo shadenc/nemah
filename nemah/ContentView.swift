29e845217d0deaadb2922ad0971e8cf1dc637e4d
Bsmah-a <my.drdbes@gmail.com>
2023-10-09 14:15:54 +0300

    Initial Commit

---
+++ nemah/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nemah
+//
+//  Created by Bsmah Ali on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nemah/nemahApp.swift
@@ -0,0 +1,17 @@
+//
+//  nemahApp.swift
+//  nemah
+//
+//  Created by Bsmah Ali on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct nemahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
