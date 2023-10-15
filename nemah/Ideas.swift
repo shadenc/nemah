c47c6e9eaacc7ff29815a7f02abd381579f473d5
Bsmah-a <my.drdbes@gmail.com>
2023-10-15 17:40:05 +0300

    new2

--- nemah/Ideas.swift
+++ nemah/Ideas.swift
@@ -12,53 +12,54 @@ struct Ideas: View {
     var recipe: [String] = ["Edaam", "Kabda", "Marasee"]
     
     var body: some View {
+        NavigationView{
         ZStack{
-            VStack{
-                Text("Not sure what do you want to cook?").font(.title).bold().padding(.top, 20.0)
-                    .foregroundColor(.white)
+           
+                VStack{
+                    Text("Not sure what do you want to cook?").font(.title).bold().padding(.top, 20.0)
+                        .foregroundColor(.white)
                     
-                
-                //Recipes Image
-                TabView(selection: $index) {
-                    ForEach((0..<3), id: \.self) { index in
-                        
-                        Image(recipe[index])
-                            .resizable().frame(width: 303,height: 455)
+                    
+                    //Recipes Image
+                    TabView(selection: $index) {
+                        ForEach((0..<3), id: \.self) { index in
+                            
+                            Image(recipe[index])
+                                .resizable().frame(width: 303,height: 455)
+                        }
                     }
-                }
-                //Carousel 000
-                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-                HStack(spacing: 2) {
-                    ForEach((0..<3), id: \.self) { index in
-                        Circle()
-                            .fill(index == self.index ? Color.white : Color.white.opacity(0.5))
-                            .frame(width: 12, height: 12)
-                        
+                    //Carousel 000
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                    HStack(spacing: 2) {
+                        ForEach((0..<3), id: \.self) { index in
+                            Circle()
+                                .fill(index == self.index ? Color.white : Color.white.opacity(0.5))
+                                .frame(width: 12, height: 12)
+                            
+                        }
                     }
-                }
-                .padding()
-                
-                Button(action: {
-                            // Button action
-                }) {
-                    HStack{
-                        Image(systemName: "wand.and.stars").resizable().padding(/*@START_MENU_TOKEN@*/.leading, 10.0/*@END_MENU_TOKEN@*/).frame(width: 30, height: 25).foregroundColor(.white)
-                        Text("Generate Recipe").foregroundColor(.white)
-                            .font(.system(size:22,weight: .bold, design: .monospaced))
-                            .padding(/*@START_MENU_TOKEN@*/.trailing, 14.0/*@END_MENU_TOKEN@*/)
-                                       }
                     .padding()
-                                       .background(Color("Button"))
-                                       .cornerRadius(20)
-                }.padding(.bottom, 25.0)
-            }
+                    
+                    NavigationLink(destination: GenerateRecipes()) {
+                                          
+                        HStack{
+                            Image(systemName: "wand.and.stars").resizable().padding(/*@START_MENU_TOKEN@*/.leading, 10.0/*@END_MENU_TOKEN@*/).frame(width: 30, height: 25).foregroundColor(.white)
+                            Text("Generate Recipe").foregroundColor(.white)
+                                .font(.system(size:22,weight: .bold, design: .monospaced))
+                                .padding(/*@START_MENU_TOKEN@*/.trailing, 14.0/*@END_MENU_TOKEN@*/)
+                        }
+                        .padding()
+                        .background(Color("Button"))
+                        .cornerRadius(20)
+                    }.padding(.bottom, 25.0)
+                }
+                
+            }.background(Color("BG"))
             
-        }.background(Color("BG"))
             
-    
+        }
     }
 }
-
 #Preview {
     Ideas()
 }
